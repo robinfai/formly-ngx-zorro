d10787a3a97edf37320ab1d030bfc9797537919e
binghuiluo <binghuiluo@tencent.com>
2018-09-19 17:51:03 +0800

    debug checkbox value sync

--- src/modules/formly-ngx-zorro/types/checkbox/checkbox-component.ts
+++ src/modules/formly-ngx-zorro/types/checkbox/checkbox-component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 
 @Component({
     selector: 'nz-formly-field-checkbox-component',
     templateUrl: './checkbox-component.html'
 })
-export class FormlyFieldCheckboxComponent extends FieldType {
+export class FormlyFieldCheckboxComponent extends FieldType implements OnChanges {
+    ngOnChanges() {
+        console.log(arguments);
+    }
 }
--- src/modules/formly-ngx-zorro/types/select/select-component.ts
+++ src/modules/formly-ngx-zorro/types/select/select-component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 
 @Component({
     selector: 'nz-formly-field-select-component',
     templateUrl: './select-component.html'
 })
-export class FormlyFieldSelectComponent extends FieldType {
+export class FormlyFieldSelectComponent extends FieldType implements OnInit {
+    ngOnInit() {
+        if (!this.to['nzMode']) {
+            this.to['nzMode'] = 'default';
+        }
+    }
 }
