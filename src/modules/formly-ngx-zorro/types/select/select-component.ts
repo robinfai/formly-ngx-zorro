71383982ac7a0cc8acf5239d1061fa1512aad14e
binghuiluo <binghuiluo@tencent.com>
2019-08-01 17:23:57 +0800

    修复 select组件默认查询方法

--- src/modules/formly-ngx-zorro/types/select/select-component.ts
+++ src/modules/formly-ngx-zorro/types/select/select-component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
+import { defaultFilterOption } from 'ng-zorro-antd/select/nz-option.pipe';
 
 @Component({
     selector: 'nz-formly-field-select-component',
     templateUrl: './select-component.html'
 })
 export class FormlyFieldSelectComponent extends FieldType implements OnInit {
+
+    defaultFilterOption = defaultFilterOption;
+
     ngOnInit() {
         if (!this.to['nzMode']) {
             this.to['nzMode'] = 'default';
